375bf621cedbd0b77b8f9182763017fa72e89e4e
Ramirez Vargas, José Pablo <webJose@gmail.com>
2025-02-23 20:31:32 -0600

    fix(core):  Correct exportation of RouterEngine

--- src/lib/core/index.ts
+++ src/lib/core/index.ts
@@ -1,2 +1,2 @@
 export { location } from "./Location.js";
-export { RouterEngine as Router, joinPaths } from "./RouterEngine.svelte.js";
+export { RouterEngine, joinPaths } from "./RouterEngine.svelte.js";
