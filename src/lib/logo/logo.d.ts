0f8daf3176fa590dc31ac99617c6fdaafb1c2978
Ramirez Vargas, José Pablo <webJose@gmail.com>
2025-09-13 23:15:17 -0600

    feat: Add library logo

---
+++ src/lib/logo/logo.d.ts
@@ -0,0 +1,14 @@
+declare module '@wjfe/n-savant/logo' {
+  const content: string;
+  export default content;
+}
+
+declare module '@wjfe/n-savant/logo64' {
+  const content: string;
+  export default content;
+}
+
+declare module '@wjfe/n-savant/logo48' {
+  const content: string;
+  export default content;
+}
