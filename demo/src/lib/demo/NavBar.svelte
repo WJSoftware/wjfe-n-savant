82af15fd993b98e714af8f094a13999514ba7e5a
José Pablo Ramírez Vargas <webJose@gmail.com>
2025-10-01 14:08:01 -0600

    chore(demo): Update demo project (#98)

--- demo/src/lib/demo/NavBar.svelte
+++ demo/src/lib/demo/NavBar.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { Link } from '@wjfe/n-savant';
+	import { Link, LinkContext } from '@wjfe/n-savant';
 
 	type Props = {
 		hash?: boolean | string;
@@ -23,62 +23,64 @@
 			<span class="navbar-toggler-icon"></span>
 		</button>
 		<div class="navbar-collapse collapse">
-			<ul class="navbar-nav" id="demonavbar">
-				<li class="nav-item">
-					<Link
-						{hash}
-						activeState={{ class: 'active', key: 'router', ariaCurrent: 'step' }}
-						class="nav-link"
-						href="/router"
-						prependBasePath>Router</Link
-					>
-				</li>
-				<li class="nav-item">
-					<Link
-						{hash}
-						activeState={{ class: 'active', key: 'route', ariaCurrent: 'step' }}
-						class="nav-link"
-						href="/route"
-						prependBasePath>Route</Link
-					>
-				</li>
-				<li class="nav-item">
-					<Link
-						{hash}
-						activeState={{ class: 'active', key: 'link', ariaCurrent: 'step' }}
-						class="nav-link"
-						href="/link"
-						prependBasePath>Link</Link
-					>
-				</li>
-				<li class="nav-item">
-					<Link
-						{hash}
-						activeState={{ class: 'active', key: 'location', ariaCurrent: 'step' }}
-						class="nav-link"
-						href="/location"
-						prependBasePath>location</Link
-					>
-				</li>
-				<li class="nav-item">
-					<Link
-						{hash}
-						activeState={{ class: 'active', key: 'routerEngine', ariaCurrent: 'step' }}
-						class="nav-link"
-						href="/router-engine"
-						prependBasePath>Router Engine</Link
-					>
-				</li>
-				<li class="nav-item">
-					<Link
-						{hash}
-						activeState={{ class: 'active', key: 'api', ariaCurrent: 'step' }}
-						class="nav-link"
-						href="/api"
-						prependBasePath>API</Link
-					>
-				</li>
-			</ul>
+			<LinkContext activeState={{ class: 'active', aria: { 'aria-current': 'step' } }}>
+				<ul class="navbar-nav" id="demonavbar">
+					<li class="nav-item">
+						<Link
+							{hash}
+							activeFor="router"
+							class="nav-link"
+							href="/router"
+							prependBasePath>Router</Link
+						>
+					</li>
+					<li class="nav-item">
+						<Link
+							{hash}
+							activeFor="route"
+							class="nav-link"
+							href="/route"
+							prependBasePath>Route</Link
+						>
+					</li>
+					<li class="nav-item">
+						<Link
+							{hash}
+							activeFor="link"
+							class="nav-link"
+							href="/link"
+							prependBasePath>Link</Link
+						>
+					</li>
+					<li class="nav-item">
+						<Link
+							{hash}
+							activeFor="location"
+							class="nav-link"
+							href="/location"
+							prependBasePath>location</Link
+						>
+					</li>
+					<li class="nav-item">
+						<Link
+							{hash}
+							activeFor="routerEngine"
+							class="nav-link"
+							href="/router-engine"
+							prependBasePath>Router Engine</Link
+						>
+					</li>
+					<li class="nav-item">
+						<Link
+							{hash}
+							activeFor="api"
+							class="nav-link"
+							href="/api"
+							prependBasePath>API</Link
+						>
+					</li>
+				</ul>
+			</LinkContext>
 		</div>
 	</div>
 </nav>
